d390ff617fd10b0546d28a481605f16adb477e47
Dan Stillman <dstillman@zotero.org>
2021-06-11 02:29:14 -0400

    Clarify expected cookie behavior

--- background/cookie-test.js
+++ background/cookie-test.js
@@ -1,15 +1,15 @@
 async function testCookies() {
-	console.log('Testing whether background fetch sends cookies');
+	console.log('Testing whether background fetch sends httpOnly cookies');
 	console.log("fetch('https://samesitetest.com/cookies/set', {credentials: 'include'})");
 	await fetch('https://samesitetest.com/cookies/set', {credentials: 'include'});
 	console.log('The following cookies have been set:');
 	let cookies = await new Promise((resolve) => {
 		chrome.cookies.getAll({domain: "samesitetest.com"}, resolve);
 	});
-	console.log(JSON.stringify(cookies));
+	console.log(JSON.stringify(cookies, null, 1));
 	console.log("fetch('https://samesitetest.com/cookies/read?test=GET', {credentials: 'include'})");
 	let result = await fetch('https://samesitetest.com/cookies/read?test=GET', {credentials: 'include'});
 	let doc = (new DOMParser()).parseFromString(await result.text(), 'text/html');
 	console.log(doc.querySelectorAll('p')[1].innerText);
-	console.log("Strict and Lax cookies should be ticks!")
+	console.log("All cookies should be present!")
 }
