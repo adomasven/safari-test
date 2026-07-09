30565a508a81808a5b88cdd9d9e0c2119581aba1
Adomas Venčkauskas <adomas.ven@gmail.com>
2026-07-09 19:21:06 +0300

    Clear output when running a different test

--- inject/run-fn.js
+++ inject/run-fn.js
@@ -2,9 +2,22 @@ function runFn(fn) {
 	chrome.runtime.sendMessage(fn)
 }
 
+function clearConsoleOutput(selector) {
+	let consoleOutput = document.querySelector(selector);
+	for (let entry of consoleOutput.querySelectorAll('p')) {
+		entry.remove();
+	}
+}
+
+function clearTestOutput() {
+	clearConsoleOutput('.output');
+	clearConsoleOutput('.bg-output');
+}
+
 let buttons = document.querySelectorAll('input');
 for (let button of buttons) {
 	button.addEventListener('click', () => {
+		clearTestOutput();
 		runFn(button.dataset.fn);
 	})
 }
